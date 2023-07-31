b5c923b2baced914a2c4f9728774bd9e5242413d
vinculando_intellij <git@github.com:SandovalBrandon1027/vinculando_intellij.git>
2023-07-30 22:09:12 -0500

    Corrigiendo archivos

--- Convertores/Datos_couchdb_a_mongodb/transportando_datos.py
+++ Convertores/Datos_couchdb_a_mongodb/transportando_datos.py
@@ -1,26 +1,26 @@
 import pymongo
 import couchdb
 
-# Conectarse a MongoDB
+# Estableciendo conexion con Mongodb
 mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
-mongo_db = mongo_client['Proyecto_analisis']  # Reemplaza 'nombre_de_tu_base_de_datos' con el nombre de tu base de datos en MongoDB
-mongo_collection = mongo_db['Bases_datos_json']  # Reemplaza 'nombre_de_tu_coleccion' con el nombre de tu colección en MongoDB
+mongo_db = mongo_client['Proyecto_analisis']  
+mongo_collection = mongo_db['Bases_datos_json']  
 
-# Conectarse a CouchDB
-couch = couchdb.Server('http://admin:admin@localhost:5984')  # Reemplaza la URL y las credenciales de CouchDB si es necesario
-couch_db = couch['servidor_principal']  # Reemplaza 'nombre_de_tu_base_de_datos_en_couchdb' con el nombre de tu base de datos en CouchDB
+# Estableciendo conexion con Couchdb 
+couch = couchdb.Server('http://admin:admin@localhost:5984')  # Modificando el url y agregando nuestras credenciales
+couch_db = couch['servidor_principal']
 
-# Obtener los documentos de MongoDB
-documents = mongo_collection.find({})  # Puedes agregar una consulta (query) aquí si deseas obtener documentos específicos
+# Exportando los datos de Mongodb
+documents = mongo_collection.find({})  
 
-# Transferir los documentos a CouchDB
+# Tranfireindo los documentos a CouchDB
 for document in documents:
-    # Eliminar la clave '_id' del documento para evitar errores al guardar en CouchDB
+    # Ya que en Mongodb nos crea un id, este debemos borrarla para que no nos cree ningun problema en Couchdb
     if '_id' in document:
         del document['_id']
 
     # Agregar el documento a CouchDB
     couch_db.save(document)
 
-# Cerrar las conexiones (opcional, pero buena práctica)
+
 mongo_client.close()
